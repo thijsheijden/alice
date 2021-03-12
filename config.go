ba3b0e94318e895faa69bc81d7a439ba4e620f1e
thijsheijden <hi@thijsheijden.nl>
2021-03-12 13:04:04 +0100

    Slightly changed config. Added setters for config properties

--- config.go
+++ config.go
@@ -6,23 +6,58 @@ import (
 
 // ConnectionConfig is a config structure to use when setting up a RabbitMQ connection
 type ConnectionConfig struct {
-	AmqpUser       string             // Username for connection
-	AmqpPassword   string             // Password for connection
-	AmqpHost       string             // URI for RabbitMQ broker
-	AmqpPort       int                // RabbitMQ broker port
-	AutoReconnect  bool               // Whether to try to reconnect after a unexpected disconnect
-	ReconnectDelay time.Duration      // The delay between reconnection attempts
-	ErrorHandler   func(<-chan error) // The error handler for the connection
+	user           string        // Username for connection
+	password       string        // Password for connection
+	host           string        // URI for RabbitMQ broker
+	port           int           // RabbitMQ broker port
+	autoReconnect  bool          // Whether to try to reconnect after a unexpected disconnect
+	reconnectDelay time.Duration // The delay between reconnection attempts
+	errorHandler   func(error)   // The error handler for the connection
 }
 
 // DefaultConfig is the default configuration for RabbitMQ.
 //	User: "guest", password: "guest", host: "localhost", port: 5672, autoReconnect: true, reconnectDelay: time.Second * 10
 var DefaultConfig = &ConnectionConfig{
-	AmqpUser:       "guest",
-	AmqpPassword:   "guest",
-	AmqpHost:       "localhost",
-	AmqpPort:       5672,
-	AutoReconnect:  true,
-	ReconnectDelay: time.Second * 10,
-	ErrorHandler:   DefaultErrorHandler,
+	user:           "guest",
+	password:       "guest",
+	host:           "localhost",
+	port:           5672,
+	autoReconnect:  true,
+	reconnectDelay: time.Second * 10,
+	errorHandler:   DefaultErrorHandler,
+}
+
+// SetUser sets the user to use for the connection to the broker
+func (config *ConnectionConfig) SetUser(user string) {
+	config.user = user
+}
+
+// SetPassword sets the password to use for the connection to the broker
+func (config *ConnectionConfig) SetPassword(password string) {
+	config.password = password
+}
+
+// SetHost sets the broker connection host URI
+func (config *ConnectionConfig) SetHost(host string) {
+	config.host = host
+}
+
+// SetPort sets the broker connection port
+func (config *ConnectionConfig) SetPort(port int) {
+	config.port = port
+}
+
+// SetAutoReconnect sets whether the connection should try reconnecting
+func (config *ConnectionConfig) SetAutoReconnect(autoReconnect bool) {
+	config.autoReconnect = autoReconnect
+}
+
+// SetReconnectDelay sets the delay between reconnection attempts
+func (config *ConnectionConfig) SetReconnectDelay(reconnectDelay time.Duration) {
+	config.reconnectDelay = reconnectDelay
+}
+
+// SetErrorHandler sets the error handler this connection uses
+func (config *ConnectionConfig) SetErrorHandler(errorHandler func(error)) {
+	config.errorHandler = errorHandler
 }
