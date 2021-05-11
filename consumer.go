1fb63210872262d5215a7d96c918957f3d3cfafe
thijsheijden <hi@thijsheijden.nl>
2021-05-11 11:58:06 +0200

    Added more attributes to consumer, added reconnect method
    Now if there is for instance a double ack the consumer will reconnect
    and start consuming messages again.

--- consumer.go
+++ consumer.go
@@ -2,25 +2,34 @@ package alice
 
 import (
 	"fmt"
+	"time"
 
 	"github.com/streadway/amqp"
 )
 
 // RabbitConsumer models a RabbitMQ consumer
 type RabbitConsumer struct {
-	channel      *amqp.Channel // Channel this consumer uses to communicate with broker
-	queue        *Queue        // The queue this consumer consumes from
-	errorHandler func(error)   // Error Handler for this consumer
-	conn         *connection   // Pointer to broker connection
+	channel        *amqp.Channel       // Channel this consumer uses to communicate with broker
+	queue          *Queue              // The queue this consumer consumes from
+	errorHandler   func(error)         // Error Handler for this consumer
+	conn           *connection         // Pointer to broker connection
+	autoAck        bool                // Whether this consumer want autoAck
+	tag            string              // Consumer tag
+	args           amqp.Table          // Additional arguments when consuming messages
+	messageHandler func(amqp.Delivery) // Message handler to call if this consumer receives a message
+	routingKey     string              // Routing key this consumer listens to
 }
 
-// ConsumeMessages consumes messages sent to the consumer
-// Keep the consumerName empty if you wish to get a randomly generated consumer name
-func (c *RabbitConsumer) ConsumeMessages(args amqp.Table, consumerName string, autoAck bool, messageHandler func(amqp.Delivery)) {
+/*
+ConsumeMessages starts the consumption of messages from the queue the consumer is bound to
+	args: amqp.Table, additional arguments for this consumer
+	messageHandler: func(amqp.Delivery), a handler for incoming messages. Every message the handler is called in a new goroutine
+*/
+func (c *RabbitConsumer) ConsumeMessages(args amqp.Table, messageHandler func(amqp.Delivery)) {
 	messages, err := c.channel.Consume(
 		c.queue.name,
-		consumerName,
-		autoAck,
+		c.tag,
+		c.autoAck,
 		false,
 		false,
 		false,
@@ -28,20 +37,28 @@ func (c *RabbitConsumer) ConsumeMessages(args amqp.Table, consumerName string, a
 	)
 	logError(err, "Failed to consume messages")
 
+	// Set some more consumer attributes
+	c.args = args
+	c.messageHandler = messageHandler
+
+	// Listen for incoming messages and pass them to the message handler
 	for message := range messages {
 		logMessage(fmt.Sprintf("Received message of '%d' bytes from exchange '%v'", len(message.Body), message.Exchange))
 		go messageHandler(message)
 	}
 }
 
-// createConsumer creates a new Consumer
-func (c *connection) createConsumer(queue *Queue, routingKey string, errorHandler func(error)) (*RabbitConsumer, error) {
+// createConsumer creates a new Consumer on this connection
+func (c *connection) createConsumer(queue *Queue, routingKey string, consumerTag string, autoAck bool, errorHandler func(error)) (*RabbitConsumer, error) {
 
 	consumer := &RabbitConsumer{
 		channel:      nil,
 		queue:        queue,
 		errorHandler: errorHandler,
 		conn:         c,
+		tag:          consumerTag,
+		autoAck:      autoAck,
+		routingKey:   routingKey,
 	}
 
 	var err error
@@ -120,14 +137,8 @@ func (c *RabbitConsumer) listenForClose() {
 	closeChan := c.channel.NotifyClose(make(chan *amqp.Error))
 	go func() {
 		closeErr := <-closeChan
-		logMessage(closeErr.Reason)
-		if closeErr.Recover {
-			logMessage("Channel close is recoverable")
-			err := c.ReconnectChannel()
-			if err != nil {
-				logMessage(fmt.Sprintf("ERROR: %s", err.Error()))
-			}
-		}
+		logError(closeErr, closeErr.Reason)
+		c.reconnect()
 	}()
 }
 
@@ -145,7 +156,57 @@ func (c *RabbitConsumer) Shutdown() error {
 	return c.channel.Close()
 }
 
+func (c *RabbitConsumer) reconnect() error {
+	// Wait for the connection to be open again
+	// Create new ticker with the desired connection delay time
+	ticker := time.NewTicker(time.Second * 10)
+
+	for {
+		<-ticker.C // New tick
+
+		// Check if connection is open yet
+		if !c.conn.conn.IsClosed() {
+			// Attempt to re-connect
+			var err error
+
+			//Connects to the channel
+			c.channel, err = c.conn.conn.Channel()
+			if err != nil {
+				return err
+			}
+
+			//Connects to exchange
+			err = c.declareExchange(c.queue.exchange)
+			if err != nil {
+				return err
+			}
+
+			//Creates the queue
+			_, err = c.declareQueue(c.queue)
+			if err != nil {
+				return err
+			}
+
+			//Binds the queue to the exchange
+			err = c.bindQueue(c.queue, c.routingKey)
+			if err != nil {
+				return err
+			}
+
+			c.listenForClose()
+
+			logMessage("consumer reconnected")
+
+			go c.ConsumeMessages(c.args, c.messageHandler)
+
+			return nil
+		}
+	}
+}
+
 // DefaultConsumerErrorHandler handles the errors of this consumer
 func DefaultConsumerErrorHandler(err error) {
 	logMessage(err.Error())
 }
+
+// MARK: Consumer errors
