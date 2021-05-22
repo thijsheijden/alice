597bab93062687d80b9ec65ec9d57d460bfc8510
thijsheijden <hi@thijsheijden.nl>
2021-05-22 17:20:25 +0200

    Updated example

--- examples/connection/main.go
+++ examples/connection/main.go
@@ -3,32 +3,47 @@ package main
 import (
 	"fmt"
 	"log"
-	"runtime"
+	"time"
 
 	"github.com/streadway/amqp"
 	"github.com/thijsheijden/alice"
 )
 
-type test struct {
-	broker alice.Broker
-}
-
 func main() {
+	// Turn on logging
 	alice.SetLogging()
-	broker, err := alice.CreateBroker(alice.DefaultConfig)
+
+	// Create a connection configuration
+	connectionConfig := alice.CreateConfig(
+		"guest",
+		"guest",
+		"localhost:5672",
+		5672,
+		true,
+		time.Second*10,
+		alice.DefaultErrorHandler,
+	)
+
+	// Create a broker using the connection config
+	broker, err := alice.CreateBroker(connectionConfig)
 	if err != nil {
 		log.Println(err)
 	}
 
+	// Create an exchange called 'test-exchange' using direct routing
 	exchange, err := alice.CreateExchange("test-exchange", alice.Direct, false, true, false, false, nil)
 	if err != nil {
 		log.Println(err)
 	}
 
+	// Create a queue called 'test-queue'
 	q := alice.CreateQueue(exchange, "test-queue", false, false, true, false, nil)
 
-	c, err := broker.CreateConsumer(q, "key", "", alice.DefaultConsumerErrorHandler)
+	// Create a consumer bound to this queue, listening for messages with routing key 'key'
+	c, err := broker.CreateConsumer(q, "key", "consumer-tag", alice.DefaultConsumerErrorHandler)
 
+	// Start consuming messages
+	// Every received message is passed to the handleMessage function
 	go c.ConsumeMessages(nil, false, handleMessage)
 
 	select {}
@@ -37,5 +52,4 @@ func main() {
 func handleMessage(msg amqp.Delivery) {
 	fmt.Println(msg.Body)
 	msg.Ack(true)
-	log.Println(fmt.Sprintf("%v", runtime.NumGoroutine()))
 }
